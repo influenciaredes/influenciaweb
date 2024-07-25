1f391adc2a626e2193b4caba37d7165fb366eaa1
Santiago Montalvo <santimontalvo@MacBook-Pro-de-Santiago.local>
2024-07-25 15:29:35 -0300

    new route

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import Home03 from "./Home03";
 
-const routes = [{ path: "/influenciaweb/home", component: <Home03 /> }];
+const routes = [{ path: "/influenciaweb", component: <Home03 /> }];
 
 export default routes;
